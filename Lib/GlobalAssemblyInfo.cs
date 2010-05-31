b2360437ae4d50572c479fe8aa7dfaee6c093e0a
Jern_cp <SND\Jern_cp>
2010-05-31 22:03:33 +0000

    First edition finished

---
+++ Lib/GlobalAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("Protect The Carrot")]
+[assembly: AssemblyCompany("Protect The Carrotters")]
+[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
